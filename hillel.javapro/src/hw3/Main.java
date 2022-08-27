3a662c6d00cbc87ccf0e73a5537422154ef2a788
AndrewEnot <103563559+AndrewEnot@users.noreply.github.com>
2022-08-27 10:32:08 +0300

    Start commit

--- hillel.javapro/src/hw3/Main.java
+++ hillel.javapro/src/hw3/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Car newOne = new Car();
         newOne.start();
+
+        SameName newSameName = new SameName();
+        newSameName.first = "Andrew";
+
+        hw3.sameOne.SameName newSameSameName = new hw3.sameOne.SameName("Andrew", "Paul", 18);
     }
 }
--- hillel.javapro/src/hw3/sameOne/SameName.java
+++ hillel.javapro/src/hw3/sameOne/SameName.java
@@ -2,8 +2,13 @@ package hw3.sameOne;
 
 public class SameName {
 
-    String first;
-    String second;
-    int last;
+    private String first;
+    private String second;
+    private int last;
 
+    public SameName(String first, String second, int last) {
+        this.first = first;
+        this.second = second;
+        this.last = last;
+    }
 }
